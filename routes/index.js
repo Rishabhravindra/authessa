b458e92b1462eaa56228cf05b89115253d5ff707
Rishabh Ravindra <rishabhravindra@gmail.com>
2017-03-15 06:57:21 -0400

    add /GET route for register and add register.pug

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
+// /GET for register
+router.get('/register', function(req,res,next) {
+	return res.render('register', {title: 'Sign Up'});
+});
+//route for index
 router.get('/', function(req,res,next) {
 	return res.render('index', { title: 'Home'});
 });
